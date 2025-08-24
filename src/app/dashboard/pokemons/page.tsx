fe4a0c1913a63e2f192b3b7aab2a101d69611c87
uriel carcacha <urielcarcacha@gmail.com>
2025-08-24 17:36:17 -0300

    continuacion del curso

---
+++ src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,30 @@
+import { PokemonGrid } from "@/app/pokemons/PokemonGrid";
+import { PokemonsReponse } from "@/app/pokemons/pokemons-response";
+import { SimplePokemon } from "@/app/pokemons/simple-pokemons";
+
+
+const getPokemons= async(limit= 20, offset= 0): Promise<SimplePokemon[]> => {
+    const data:PokemonsReponse= await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+     .then(res=> res.json())
+	
+	const pokemons= data.results.map(pokemon=>({
+		id: pokemon.url.split('/').at(-2)!, 	//obtener el id de la url del pokemon, el ! es para que no tire error
+		name: pokemon.name
+	}))
+    return pokemons
+}
+
+
+
+export default async function PokemonsPage() {
+
+    const pokemons= await getPokemons(150)
+	
+  return (
+	<div className="flex flex-col">
+		<span className="text-5xl my-2">Listado de Pokémons <small>estático</small></span>
+		<PokemonGrid pokemons={ pokemons } />
+
+  	</div>
+  );
+}
