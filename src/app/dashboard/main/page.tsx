05167b44f8a482746e4b7d009316f98446c98966
uriel carcacha <urielcarcacha@gmail.com>
2025-08-29 20:53:23 -0300

    widgetsgrid en mainpage

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,8 +1,20 @@
+import { WidgetsGrid } from "@/app/components/WidgetsGrid";
+
+
+
+export const metadata = {
+ title: 'Admin Dashboard',
+ description: 'SEO Title',
+};
 
 export default function MainPage() {
   return (
-    <div>
-      <h1>Main Page</h1>
+    <div className="text-black p-2">
+      <h1 className="mt-2 text-3xl">Dashboard</h1>
+      <span className="text-xl">Información general</span>
+
+      <WidgetsGrid/>{/*contador de productos agregados */}
+
     </div>
   );
 }
